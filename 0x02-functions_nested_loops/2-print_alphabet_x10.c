b3f607f297ef3cd3a3ad776da9b82341167b8400
Mohamed Malainine <malaininemohamed1@gmail.com>
2023-03-22 14:08:09 -0700

    Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
-	void print_alphabet_x10(void)
-	{
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
 		char ch;
 		int i;
 
@@ -21,8 +21,3 @@
 			{
 				_putchar(ch);
 				ch++;
-			}
-			_putchar('\n');
-			i++;
-		}
-	}
